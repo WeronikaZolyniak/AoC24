08a3f9b3a7e19d91153aebcd6403b0ad3e6870f4
WeronikaZolyniak <zolyniak.weronika@gmail.com>
2024-12-16 21:21:25 +0100

    day 8 part 2

--- Source/AoC24/Day8.cpp
+++ Source/AoC24/Day8.cpp
@@ -34,10 +34,10 @@ int UDay8::ReturnAnswer(FString String)
 		Antinodes.Add(line_array);
 	}
 
-	//UE_LOG(LogTemp, Error, TEXT("iiii"));
-
 	int sum = 0;
-	for (int y = 0; y < length; y++)
+
+	//part 1
+	/*for (int y = 0; y < length; y++)
 	{
 		for (int x = 0; x < width; x++)
 		{
@@ -96,6 +96,90 @@ int UDay8::ReturnAnswer(FString String)
 			}
 		}
 	}
+
+	for (int y = 0; y < length; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			if (Antinodes[y][x] == '#')
+			{
+				sum++;
+			}
+		}
+	}
+
+	return sum;*/
+
+	//part 2
+	for (int y = 0; y < length; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			//check for antenna
+			if (Array[y][x] != '.')
+			{
+				char antenna_frequency = Array[y][x];
+				for (int i = 0; i < length; i++)
+				{
+					for (int j = 0; j < width; j++)
+					{
+						if (i == y && j == x) continue;
+						//check for the same frequency
+						if (Array[y][x] == Array[i][j])
+						{
+							Antinodes[y][x] = '#';
+							int antinode_x = abs(x - j);
+							int antinode_y = abs(y - i);
+
+							//all options of placement
+							if (y < i)
+							{
+								if (x < j)
+								{
+									antinode_y = -antinode_y;
+									antinode_x = -antinode_x;
+								}
+								else
+								{
+									antinode_y = -antinode_y;
+								}
+							}
+							else
+							{
+								if (x < j)
+								{
+									antinode_x = -antinode_x;
+								}
+							}
+
+							//antinode from main antenna
+							int initial_antinode_x = antinode_x;
+							int initial_antinode_y = antinode_y;
+							while (ValuesAreOnArray(x + antinode_x, y + antinode_y, length, width))
+							{
+								Antinodes[y + antinode_y][x + antinode_x] = '#';
+								antinode_x += initial_antinode_x;
+								antinode_y += initial_antinode_y;
+							}
+
+							//antinode from checked antenna
+							int sec_antinode_y = -antinode_y;
+							int sec_aninode_x = -antinode_x;
+							initial_antinode_x = sec_aninode_x;
+							initial_antinode_y = sec_antinode_y;
+							while (ValuesAreOnArray(j + sec_aninode_x, i + sec_antinode_y, length, width))
+							{
+								Antinodes[i + sec_antinode_y][j + sec_aninode_x] = '#';
+								sec_aninode_x += initial_antinode_x;
+								sec_antinode_y += initial_antinode_y;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 
 	for (int y = 0; y < length; y++)
 	{
